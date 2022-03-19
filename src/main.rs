c16d99cda98965f73738b58197812d79e8a61e6d
JadKHaddad <jadkhaddad@gmail.com>
2022-03-19 23:40:37 +0100

    decode/encode

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,33 @@ use std::fs::File;
 use std::io::Read;
 use colored::*;
 
+static ENCODE_FILE: &str = "morse-code-encode.json";
+static DECODE_FILE: &str = "morse-code-decode.json";
+
+pub struct CustomError {
+    
+}
+
+pub fn json_from_file(path: &str) -> Json{
+    let mut file = match File::open(path) {
+        Ok(ok) => ok,
+        Err(_) => {
+            println!("Error: Could not find file: {}", path);
+            process::exit(1);
+        }
+    };
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    let json = match Json::from_str(&data)  {
+        Ok(ok) => ok,
+        Err(_) => {
+            println!("Error: Encode file is not valid: {}", path);
+            process::exit(1);
+        }
+    };
+    return json;
+}
+
 pub fn utf8_slice(s: &str, start: usize, end: usize) -> Option<&str> {
     let mut iter = s.char_indices()
         .map(|(pos, _)| pos)
@@ -18,53 +45,73 @@ pub fn utf8_slice(s: &str, start: usize, end: usize) -> Option<&str> {
     Some(&s[start_pos..*iter.peek()?])
 }
 
+pub fn encode(input: &str) -> Result<String, CustomError> {
+    let json = json_from_file(ENCODE_FILE);
+    let clean_input = Regex::new(r"\s+").unwrap().replace_all(&input, " ").to_string();
+    let clean_input_len = clean_input.chars().count();
+    let mut vec = vec![String::new(); clean_input_len];
+    let clean_input_slice = utf8_slice(&clean_input, 0, clean_input_len - 1).unwrap();
+    let mut success = true; 
+    for (i, c) in  clean_input_slice.chars().enumerate() {
+        if c == '\n' {
+            break;
+        }
+        let code_char = unescape(& match json.find_path(&[&c.to_string()]){
+            Some(ok) => ok,
+            None => {
+                println!("The fuck is this {} ? Try again", c.to_string().red());
+                success = false;
+                break;
+            }
+        }.to_string()).unwrap();
+        vec[i] = code_char;
+    }
+    if success {
+        let joined = vec.join(" ");
+        return Ok(joined);
+    }
+    Err(CustomError{})
+}
+
+pub fn decode(input: &str) -> Result<String, CustomError> {
+    let json = json_from_file(DECODE_FILE);
+    let clean_input = Regex::new(r"\s+").unwrap().replace_all(&input, " ").to_string();
+    let clean_input = Regex::new(r"/+").unwrap().replace_all(&clean_input, "/").to_string();
+    let split = clean_input.split(" ");
+    let words: Vec<&str> = split.collect();
+    let mut vec = vec![String::new(); words.len() - 1];
+    let mut success = true; 
+    for i in 0..words.len() - 1 {
+        let code_char = unescape(& match json.find_path(&[&words[i].to_string()]){
+            Some(ok) => ok,
+            None => {
+                println!("The fuck is this {} ? Try again", words[i].to_string().red());
+                success = false;
+                break;
+            }
+        }.to_string()).unwrap();
+        vec[i] = code_char;
+    }
+    if success {
+        let joined = vec.join("");
+        return Ok(joined);
+    }
+    Err(CustomError{})
+}
+
 fn main() {
-    let encode_file = "morse-code-decode.json";
-    let mut file = match File::open(encode_file) {
-        Ok(ok) => ok,
-        Err(_) => {
-            println!("Error: Could not find encode file: {}", encode_file);
-            process::exit(1);
-        }
-    };
-    let mut data = String::new();
-    file.read_to_string(&mut data).unwrap();
-    let json = match Json::from_str(&data)  {
-        Ok(ok) => ok,
-        Err(_) => {
-            println!("Error: Encode file is not valid");
-            process::exit(1);
-        }
-    };
-    
     loop{
         println!("Input:");
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("Error: Failed to read line");
-        let clean_input = Regex::new(r"\s+").unwrap().replace_all(&input, " ").to_string();
-        let clean_input_len = clean_input.chars().count();
-        let mut vec = vec![String::new(); clean_input_len];
-        let clean_input_slice = utf8_slice(&clean_input, 0, clean_input_len - 1).unwrap();
-        let mut success = true; 
-        for (i, c) in  clean_input_slice.chars().enumerate() {
-            //println!("char: {:?}", c);
-            if c == '\n' {
-                break;
-            }
-            let code_char = unescape(& match json.find_path(&[&c.to_string()]){
-                Some(ok) => ok,
-                None => {
-                    println!("The fuck is this {} ? Try again", c.to_string().red());
-                    success = false;
-                    break;
-                }
-            }.to_string()).unwrap();
-            vec[i] = code_char;
-            //print!("{}", json.find_path(&[&c.to_string()]).unwrap());
-        }
-        if success {
-            let joined = vec.join(" ");
-            println!("\nOutput: {}", joined.green());
-        }
+        /*match encode(&input) {
+            Ok(ok) => println!("\nOutput: {}", ok.green()),
+            Err(_) => continue
+        };
+        */
+        match decode(&input) {
+            Ok(ok) => println!("\nOutput: {}", ok.green()),
+            Err(_) => continue
+        };
     }
 }
