ff5870b69bab9ad9f8ebcae9d3528574cd64422d
JadKHaddad <jadkhaddad@gmail.com>
2022-03-20 00:48:08 +0100

    TODO: remove warnings, add dynamic parser, add dockerfile

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ pub fn encode(input: &str) -> Result<String, CustomError> {
     let clean_input = Regex::new(r"\s+").unwrap().replace_all(&input, " ").to_string();
     let clean_input_len = clean_input.chars().count();
     let mut vec = vec![String::new(); clean_input_len];
-    let clean_input_slice = utf8_slice(&clean_input, 0, clean_input_len - 1).unwrap();
+    let clean_input_slice = utf8_slice(&clean_input, 0, clean_input_len).unwrap();
     let mut success = true; 
     for (i, c) in  clean_input_slice.chars().enumerate() {
         if c == '\n' {
@@ -80,9 +80,9 @@ pub fn decode(input: &str) -> Result<String, CustomError> {
     let clean_input = Regex::new(r"/+").unwrap().replace_all(&clean_input, "/").to_string();
     let split = clean_input.split(" ");
     let words: Vec<&str> = split.collect();
-    let mut vec = vec![String::new(); words.len() - 1];
+    let mut vec = vec![String::new(); words.len()];
     let mut success = true; 
-    for i in 0..words.len() - 1 {
+    for i in 0..words.len(){
         let code_char = unescape(& match json.find_path(&[&words[i].to_string()]){
             Some(ok) => ok,
             None => {
@@ -100,6 +100,10 @@ pub fn decode(input: &str) -> Result<String, CustomError> {
     Err(CustomError{})
 }
 
+pub fn print_not_valid_input(){
+    println!("{} Use -h for more info", "Input is not valid.".red());
+}
+
 fn main() {
     let matches = App::new("Morsecode Encoder Decoder")
         .version("1.0")
@@ -107,33 +111,44 @@ fn main() {
                  .short('e')
                  .long("encode")
                  .takes_value(true)
-                 .help("Encode string"))
+                 .help("Encode string. Usage: -e \"[string]\""))
         .arg(Arg::with_name("decode")
                  .short('d')
                  .long("decode")
                  .takes_value(true)
-                 .help("Decode morsecode"))
+                 .help("Decode morsecode. Usage: -d \"[-. .. -.-. .]\""))
         .get_matches();
 
     let encode_val = matches.value_of("encode").unwrap_or("");
-    let decode_val  = matches.value_of("decode").unwrap_or("");
+    let decode_val = matches.value_of("decode").unwrap_or("");
+    let re = Regex::new(r"^\[.*?\]$").unwrap();
     if encode_val != "" {
-        match encode(encode_val) {
-            Ok(ok) => {
-                println!("{}", ok.green());
-                process::exit(1);
-            },
-            _ => process::exit(1)
-        };
+        if re.is_match(encode_val){
+            match encode(&encode_val[1..encode_val.chars().count() - 1]) {
+                Ok(ok) => {
+                    println!("{}", ok.green());
+                    process::exit(1);
+                },
+                _ => process::exit(1)
+            };
+        }
+        else{
+            print_not_valid_input(); 
+        }
     }
     else if decode_val != "" {
-        match decode(decode_val) {
-            Ok(ok) => {
-                println!("{}", ok.green());
-                process::exit(1);
-            },
-            _ => process::exit(1)
-        };
+        if re.is_match(decode_val){
+            match decode(&decode_val[1..decode_val.chars().count() - 1]) {
+                Ok(ok) => {
+                    println!("{}", ok.green());
+                    process::exit(1);
+                },
+                _ => process::exit(1)
+            };
+        }
+        else{
+            print_not_valid_input();
+        }
     }
     else {
         loop{
